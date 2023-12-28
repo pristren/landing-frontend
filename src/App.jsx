f0144fe6598b25592b7f71d8da1e2daa17f3536b
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-12-28 23:54:52 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { AccordionDemo } from "./AccordionDemo";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+      <AccordionDemo />
+    </>
+  );
+}
+
+export default App;
