eee00b30a3453a4146b2991c8de8cd93b7e2d128
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2024-01-04 18:27:07 +0600

    updated

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -6,11 +6,14 @@ import { cn } from "@/lib/utils";
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn("relative h-4 w-full   rounded-full bg-white ", className)}
+    className={cn(
+      "relative h-3 w-full  overflow-hidden rounded-full bg-[#3b3b3b] ",
+      className
+    )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full  flex-1 overflow-hidden bg-black  transition-transform duration-1000 ease-[cubic-bezier(0.65, 0, 0.35, 1)] "
+      className="h-full w-full  flex-1 overflow-hidden bg-slate-50  transition-transform duration-1000 ease-[cubic-bezier(0.65, 0, 0.35, 1)] "
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
